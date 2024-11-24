6023ea6062825d6df82f9c3e5e8ff0c07d0971fd
James Waugh <james.waugh.r@gmail.com>
2024-11-23 19:43:51 -0500

    New horizons?

--- software/src/thermo-app/src/setpoint.rs
+++ software/src/thermo-app/src/setpoint.rs
@@ -1,1 +1,18 @@
+struct State {
+    setpoint: u32,
+}
 
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

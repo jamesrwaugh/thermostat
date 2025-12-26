85478b0baae1582e56ccdf51b3341a047111f426
James <james.waugh.r@gmail.com>
2025-12-25 20:26:06 -0500

    WIP

--- software/src/app-screen/main.cpp
+++ software/src/app-screen/main.cpp
@@ -6,6 +6,7 @@
 
 #include "ThermoSaveData_bp.h"
 #include "twi_master.h"
+#include "util/delay.h"
 
 // 112 x 16
 // Temp Display Unit
@@ -23,9 +24,9 @@ const uint8_t gUnderlineImageData[5] = {
 
 void EnterTemp(Noritake_VFD_GU7000& s) {
   s.print("TEMP");
-  // s.GU7000_drawImage(0, 16, 5, 8, gUnderlineImageData);
   s.GU7000_setCursor(0, 8);
   s.print("C");
+  s.GU7000_drawImage(1, 8, 5, 8, gUnderlineImageData);
 }
 
 class Program {};
@@ -47,7 +48,7 @@ void setup() {
   // Set prescaler value of 1
   TWSR &= ~_BV(TWPS0);
   TWSR &= ~_BV(TWPS1);
-  TWBR = ((F_CPU / 50000) - 16) / 2;
+  TWBR = ((F_CPU / 250000) - 16) / 2;
 
   // Screen Busy
   // -> Input
@@ -73,6 +74,15 @@ int main() {
   {
     AutoTwi twi;
     EnterTemp(s);
+  }
+
+  while (1) {
+    AutoTwi twi;
+    _delay_ms(500);
+    s.GU7000_setCursor(0, 8);
+    s.print("C");
+    _delay_ms(500);
+    s.GU7000_drawImage(0, 7, 5, 8, gUnderlineImageData);
   }
 
   return 0;

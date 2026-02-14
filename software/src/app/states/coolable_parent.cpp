b51b9b9d872138b2676c57833f5dffa8b2e52830
James <james.waugh.r@gmail.com>
2026-02-14 17:41:51 -0500

    I like this a little better

--- software/src/app/states/coolable_parent.cpp
+++ software/src/app/states/coolable_parent.cpp
@@ -211,7 +211,7 @@ bool FrictionScrollManager::AttemptScroll() {
       scroll_attempts_ = 0;
       s_.ScrollOnce();
       bool addAggressive = s_.RemainingLines() < (Image2xHeight / 2);
-      current_friction_ = max(20, current_friction_ + (addAggressive ? 24 : 1));
+      current_friction_ = max(20, current_friction_ + (addAggressive ? 20 : 1));
       return true;
     }
   } else {

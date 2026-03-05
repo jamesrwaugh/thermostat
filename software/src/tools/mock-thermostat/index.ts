fbf9abce8691d6d7748efec90a162a40b1e719f6
James <james.waugh.r@gmail.com>
2026-03-04 20:50:52 -0500

    Rust gets nuked again. Bring in CPP and esp-idf for comms. Set up clangd

---
+++ software/src/tools/mock-thermostat/index.ts
@@ -0,0 +1,28 @@
+const ConfigUrl = "homeassistant/climate/home123/config";
+
+const Payload = {
+  dev: {
+    ids: "ea334450945afc",
+    name: "Home",
+    mf: "Squared Elecronic",
+    mdl: "zero",
+    sw: "1.0",
+    sn: "0002",
+    hw: "1.0",
+  },
+  o: {
+    name: "bla2mqtt",
+    sw: "2.1",
+    url: "https://bla2mqtt.example.com/support",
+  },
+  action_topic: "study/ac/power/set",
+  current_humidity_topic: "study/ac/preset_mode/set",
+  current_temperature_topic: "study/ac/mode/set",
+  fan_mode_command_topic: "study/ac/fan/set",
+  fan_mode_state_topic: "study/ac/fan/set",
+  mode_command_topic: "a",
+  mode_state_topic: "b",
+  power_command_topic: "w",
+  temperature_command_topic: "s",
+  precision: 0.5,
+};

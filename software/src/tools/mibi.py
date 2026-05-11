dcdfca2b62e05f079c4def2aea4b98298dccafc4
James <james.waugh.r@gmail.com>
2026-05-11 19:59:27 -0400

    New Mibi Tool with 2^8 factor

--- software/src/tools/mibi.py
+++ software/src/tools/mibi.py
@@ -10,14 +10,17 @@ import sys
 # 1: 1/256
 # 0: 1/512
 
+mibiFactor = 8
 number = int(sys.argv[1])
-wholePart = number >> 9
+wholePart = number >> mibiFactor
 fracPart = 0
 
-for i in range(0, 9):
+print(wholePart)
+
+for i in range(0, mibiFactor):
     bit = 1 if number & (1 << i) else 0
     if bit:
-        item = (1 / (2 ** (9 - i)))
+        item = 1 / (2 ** (mibiFactor - i))
         fracPart += item
         print(f"{i}: {item}")
 
